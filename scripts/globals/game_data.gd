6d35e40a361ae1129b535937563757b8611b11a5
Zachary Lounsberry <zlounsberry@gmail.com>
2026-04-18 06:53:28 -0400

    hacking away

---
+++ scripts/globals/game_data.gd
@@ -0,0 +1,59 @@
+extends Node
+
+const HORF_DICT: Dictionary = {
+	0: {
+		"odds_to_one": 1,
+		"low_speed": 15,
+		"high_speed": 30,
+		"lock_in_likelihood": 0.45,
+	},
+	1: {
+		"odds_to_one": 2,
+		"low_speed": 14,
+		"high_speed": 27.5,
+		"lock_in_likelihood": 0.45,
+	},
+	2: {
+		"odds_to_one": 4,
+		"low_speed": 13,
+		"high_speed": 22.5,
+		"lock_in_likelihood": 0.45,
+	},
+	3: {
+		"odds_to_one": 8,
+		"low_speed": 10,
+		"high_speed": 20,
+		"lock_in_likelihood": 0.4,
+	},
+	4: {
+		"odds_to_one": 16,
+		"low_speed": 7.5,
+		"high_speed": 20,
+		"lock_in_likelihood": 0.35,
+	},
+	5: {
+		"odds_to_one": 24,
+		"low_speed": 5,
+		"high_speed": 17.5,
+		"lock_in_likelihood": 0.35,
+	},
+	6: {
+		"odds_to_one": 32,
+		"low_speed": 4,
+		"high_speed": 17.5,
+		"lock_in_likelihood": 0.3,
+	},
+	7: {
+		"odds_to_one": 64,
+		"low_speed": 4,
+		"high_speed": 12.5,
+		"lock_in_likelihood": 0.2,
+	},
+}
+
+@onready var current_money: int = 5000
+@onready var current_day: int = 1
+
+@onready var main_volume: float = 0.8
+@onready var sfx_volume: float = 0.8
+@onready var music_volume: float = 0.8
--- scripts/horf.gd
+++ scripts/horf.gd
@@ -2,26 +2,37 @@ extends Node2D
 
 const VELOCITY_UPDATES: int = 5
 
-@export var speed_low: float = 8
-@export var speed_high: float = 12
-@export var speed_modifier: float = 1.0
-@export var horf_id: int = 1
-@export var lock_in_likelihood: float = 0.2
-
 @onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
 @onready var change_speed_timer: Timer = $ChangeSpeed
 @onready var lock_in_timer: Timer = $LockIn
-@onready var horse_anim: AnimationPlayer = $HorseAnim
+@onready var horf_anim: AnimationPlayer = $HorfAnim
 @onready var emote_anim: AnimationPlayer = $EmoteAnim
 @onready var race_over: bool = false
 @onready var locked_in: bool = false
 
+var speed_modifier: float = 1.0
 var speed: float
+var speed_low: float
+var speed_high: float
+var lock_in_likelihood: float
+var horf_id: int
+var odds_to_one: int
+
+var modifier_delta_on_bribe: int
+var lock_in_delta_on_bribe: int = randf_range(0.025, 0.05)
+var cost_to_bribe: int = randi_range(100, 500)
 
 
 func _ready() -> void:
 	_update_current_speed()
 	_connect_signals() 
+	if not GameData.HORF_DICT.has(horf_id):
+		print("error, misassigned horf dict")
+		return
+	odds_to_one = GameData.HORF_DICT[horf_id]["odds_to_one"]
+	speed_low = GameData.HORF_DICT[horf_id]["low_speed"]
+	speed_high = GameData.HORF_DICT[horf_id]["high_speed"]
+	lock_in_likelihood = GameData.HORF_DICT[horf_id]["lock_in_likelihood"]
 
 
 func _connect_signals() -> void:
@@ -43,8 +54,19 @@ func _update_current_speed() -> void:
 	speed = new_speed
 
 
+func _bribe_jockey(is_bribed_to_be_worse: bool) -> void:
+	if is_bribed_to_be_worse:
+		speed_modifier -= modifier_delta_on_bribe
+		lock_in_likelihood -= lock_in_delta_on_bribe
+	else:
+		speed_modifier += modifier_delta_on_bribe
+		lock_in_likelihood += lock_in_delta_on_bribe
+	speed_low *= speed_modifier
+	speed_high *= speed_modifier
+
+
 func _race_start() -> void:
-	horse_anim.play("run")
+	horf_anim.play("run")
 
 
 func _race_over(_horf_id: int) -> void:
@@ -54,7 +76,6 @@ func _race_over(_horf_id: int) -> void:
 func _on_misfire_gun() -> void:
 	if locked_in:
 		return
-	
 	speed = 0
 	change_speed_timer.start()
 
